b335574cc8017e4ae3d1fcfc3d19c383af2eb655
Francie <99582010+frncie-404@users.noreply.github.com>
2024-10-31 17:51:46 +0800

    Update extra-bs.js

--- extra-bs.js
+++ extra-bs.js
@@ -13,18 +13,3 @@ var scrollSpy = new bootstrap.ScrollSpy(document.body, {
 $(window).on('hashchange', function(e){
   history.replaceState ("", document.title, e.originalEvent.oldURL);
 });
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-});
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-});
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-});
