16170fa3181d36e1ef073ab5133b054c03c490c7
Francie <99582010+frncie-404@users.noreply.github.com>
2024-10-31 17:51:23 +0800

    Update extra-bs.js
    Fully updated commissions page

--- extra-bs.js
+++ extra-bs.js
@@ -1,10 +1,30 @@
+(function () {
+  'use strict'
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})();
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#scrollable'
+});
+
+$(window).on('hashchange', function(e){
+  history.replaceState ("", document.title, e.originalEvent.oldURL);
+});
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
-})
+});
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
